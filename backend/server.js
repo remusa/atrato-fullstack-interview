97a3d4170c533ed8677d835daa2f7ae14d1a944f
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-05-31 15:01:35 -0600

    chore: add body-parser & dotenv
    chore: get mock users data

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const data = require("../data/data.json");
 const PORT = 5000;
 
+let users = data;
+
 app.listen(5000, () => {
-  console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
 
-app.get('/', function(req, res){
-    res.send('Hello from backend')    
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello from backend");
+});
