3da9ab69a90e24a1b4424c61e9860cf834d038c3
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-05-31 15:03:42 -0600

    feat: get, create and delete users endpoints
    chore: add auth token

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { getNewUserId, getCreditCard } = require("./utils");
 
 require("dotenv").config();
 
@@ -18,4 +19,39 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 	res.send("Hello from backend");
+});
+
+app.use((req, res, next) => {
+	const authToken = req.headers.authorization?.split("Bearer")?.at(1).trim();
+	if (!authToken) {
+		return res.status(403).json({ error: "Not authorized" });
+	}
+	if (authToken !== process.env.API_KEY) {
+		return res.status(403).json({ error: "Invalid authorization token" });
+	}
+	next();
+});
+
+app.get("/v1/users", (req, res) => {
+	res.status(200).json({ data: users, total: users.length });
+});
+
+app.post("/v1/users", async (req, res) => {
+	const data = req.body;
+	const { user } = data;
+	const id = getNewUserId();
+	const creditCardData = await getCreditCard();
+	const newUser = { id, ...user, cardInfo: creditCardData };
+	users = users.concat(newUser);
+	res.status(201).json({
+		message: "User created",
+	});
+});
+
+app.delete("/v1/users/:id", (req, res) => {
+	const { id } = req.params;
+	users = users.filter((user) => {
+		return Number.parseInt(user.id) !== Number.parseInt(id);
+	});
+	res.status(200).json({ message: `User with id: ${id} succesfully deleted` });
 });
---
+++ backend/utils.js
@@ -0,0 +1,24 @@
+const { default: axios } = require("axios");
+
+function getNewUserId() {
+	const id = Math.floor(Math.random(0, 1) * 1000);
+	return id;
+}
+
+const options = {
+	method: "GET",
+	url: process.env.CARD_API_URL,
+	headers: { "X-Api-Key": process.env.CARD_API_KEY },
+};
+
+async function getCreditCard() {
+	try {
+		const { data } = await axios.request(options);
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+exports.getNewUserId = getNewUserId;
+exports.getCreditCard = getCreditCard;
